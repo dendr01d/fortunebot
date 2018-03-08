c77ccc0532d327b0eda79e372bdaa10d9f4be202
Duncan Miller <duncanfmiller@gmail.com>
2018-03-08 14:43:01 -0500

    Updated main.py to work with markov2 and the new savefiles

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import tweepy
 import time
 import random
 from math import ceil
+import pickle
 
-from markov import *
+from markov2 import *
 from cred_sensitive import *
 
 # authorization
@@ -11,22 +12,55 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+corpora = [
+'singleFortune.txt',
+'EICorpus.txt',
+'handmadeCorpus.txt',
+'wikiCorpus.txt']
 
-# instantiate the graph
+
+print('If you want to load a file, what\'s it\'s name?')
+fileName = input('?>')
+
+web = {}
 chain = Graph()
-chain.feedCorpus('EICorpus.txt')
+
+if fileName != '':
+	try:
+		web = pickle.load(open(fileName, 'rb'))
+		print('File loaded')
+		chain.load(web)
+
+	except IOError as e:
+		print('Could not open \'{}\':'.format(fileName))
+		print('I/O error ({}): {}'.format(e.errno, e.strerror))
+
+# if nothing's been loaded into the web
+elif web == {}:
+	print('Create a new model from scratch?')
+	cmd = input('?>')
+
+	if cmd[0] == 'y':
+		print('Loading corpora...')
+		for f in corpora:
+			chain.feedCorpus(f)
+	else:
+		print('Well then what DO you want?')
+		exit()
+
+print('Okay time to start tweeting')
+
 
 while(1):
 	msg = chain.generate()
-	print('New message: {}'.format(msg))
 
 	try:
-		print('[{}]'.format(time.strftime('%X')))	
+		print('[{}] {}'.format(time.strftime('%X'), msg))	
 		print('Sending tweet...')
 		api.update_status(msg)
 		print('Success!')
 
-		naptime = random.randint(900, 1800)
+		naptime = random.randint(900, 1200)
 		print('Sleeping for ~{} minutes'.format(ceil(naptime/60)))
 		time.sleep(naptime)
 
